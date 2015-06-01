b358f6f7764dfea65ef0c61ad563c44df9a19307
Arnab Chaudhuri <coolguyforgenerationx@gmail.com>
2015-06-01 17:29:43 +0530

    Initial DirtyUnicorns BringUp

--- du.mk
+++ du.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/du/config/common.mk)
+
 # Release name
 PRODUCT_RELEASE_NAME := GT-I9100
 
@@ -21,15 +24,15 @@ PRODUCT_RELEASE_NAME := GT-I9100
 TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common DU stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/i9100/full_i9100.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9100
-PRODUCT_NAME := cm_i9100
+PRODUCT_NAME := du_i9100
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := GT-I9100
 
